5fed970316f8ce7f6b06249b2d6583da530ea81d
gayathri perumal <gayathriperumal100@gmail.com>
2020-04-26 21:00:22 +0530

    Updates

---
+++ precache-manifest.4f006207aaeb058dd27a17f6536dc95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfb874d6afeb13c44c0f0049c935690",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cb48e2f554df23805ab4",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3a51ed187374089e7d0",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb48e2f554df23805ab4",
+    "url": "/portfolio/static/js/main.9ee62c46.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
